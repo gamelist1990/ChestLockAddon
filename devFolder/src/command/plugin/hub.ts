cb22c88b2b9c1aeefd152a5c45c9261e3e6a0d51
Koukunn_ <testbing@outlook.jp>
2024-11-22 12:55:19 +0900

    hubのy座標のバグを修正

--- devFolder/src/command/plugin/hub.ts
+++ devFolder/src/command/plugin/hub.ts
@@ -1,6 +1,6 @@
 import { config } from '../../Modules/Util';
 import { registerCommand, verifier } from '../../Modules/Handler';
-import { Player, system, world } from '@minecraft/server';
+import { Player, system, Vector3, world } from '@minecraft/server';
 import { translate } from '../langs/list/LanguageManager';
 
 registerCommand({
@@ -14,14 +14,21 @@ registerCommand({
         let countdown = 5; // カウントダウンの開始値
 
         const intervalId = system.runInterval(() => {
-            player.sendMessage(translate(player,"command.hub.move",{countdown:`${countdown}`}))
+            player.sendMessage(translate(player, "command.hub.move", { countdown: `${countdown}` }));
             countdown--;
 
             if (countdown < 0) {
                 system.clearRun(intervalId);
                 const Default = world.getDefaultSpawnLocation();
-                player.teleport(Default);
+
+                const teleportLocation: Vector3 = {
+                    x: Default.x,
+                    y: Default.y !== undefined && Default.y !== null ? Default.y : 64,
+                    z: Default.z
+                };
+
+                player.teleport(teleportLocation);
             }
-        }, 20); 
+        }, 20);
     },
 });
