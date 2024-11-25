508a41ef430c46893babd2feff9febe1f24195a1
Koukunn_ <testbing@outlook.jp>
2024-11-25 14:12:36 +0900

    Update Copy Plugin

--- tool/config.js
+++ tool/config.js
@@ -1,8 +1,21 @@
+import path from 'path';
+import os from 'os';
 
+const minecraftDataPath = path.join(
+    os.homedir(),
+    'AppData',
+    'Local',
+    'Packages',
+    'Microsoft.MinecraftUWP_8wekyb3d8bbwe',
+    'LocalState',
+    'games',
+    'com.mojang'
+);
 
 const config = {
     path: "./devFolder/scripts/devFolder/src",
-    copyToPath: "./test"
+    copyToPath: `${minecraftDataPath}/minecraftWorlds/dfKxVMbKNUo=/behavior_packs/createAddon/scripts`
 
 };
+
 export default config;
