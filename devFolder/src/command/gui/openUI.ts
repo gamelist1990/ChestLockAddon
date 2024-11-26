b92cab74ddaffb25ff8aa586a228d899889d7b8b
Koukunn <testbing@outlook.jp>
2024-11-26 16:43:23 +0900

    Update

--- devFolder/src/command/gui/openUI.ts
+++ devFolder/src/command/gui/openUI.ts
@@ -1,4 +1,4 @@
-import { closeForm, config } from '../../Modules/Util';
+import { config } from '../../Modules/Util';
 import { registerCommand, verifier } from '../../Modules/Handler';
 import { showBasicUI } from './ui';
 import { Player, system } from '@minecraft/server';
@@ -13,9 +13,8 @@ registerCommand({
   require: (player: Player) => verifier(player, config().commands['ui']),
   executor: (player: Player) => {
     player.sendMessage(translate(player, 'server.closeChat'));
-    system.runTimeout(() => {
-      closeForm(player);
+    system.runTimeout(()=>{
       showBasicUI(player);
-    }, 0);
+    },20 * 5)
   },
 });
